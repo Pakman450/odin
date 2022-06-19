db741e918a6cbfa8165f035914a7fb826346b411
Steven Pak <stevenpak@stevens-mbp.lan>
2022-06-19 14:49:28 -0400

    rearranging git folders

---
+++ 03_js/javascript.js
@@ -0,0 +1,31 @@
+
+function add7(num) {
+
+    var tot = num + 7;
+
+    return tot;
+} 
+
+
+console.log(add7(5))
+
+
+
+function capitalize(string) {
+
+
+    return string.toUpperCase();
+}
+
+console.log(capitalize("asdE"))
+
+
+function lastLetter(string_1){
+
+
+
+    return string_1[string_1.length - 1]
+}
+
+
+console.log(lastLetter("asdf"))

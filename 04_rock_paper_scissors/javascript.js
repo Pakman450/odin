975ec9510e59490298b81aed3e70a1a0be9f3ecb
Steven Pak <steven.pak10@gmail.com>
2022-06-21 23:07:52 -0400

    got the console.log to work dealing with rps

--- 04_rock_paper_scissors/javascript.js
+++ 04_rock_paper_scissors/javascript.js
@@ -31,46 +31,45 @@ function playRound(e, computerin) {
     var result = "";
 
     //needed to make sure that the player's input is all different capitalizations.
-    console.log(e.id)
+    // console.log(e.target.id)
     // playerin = e.id
     // playerin.toLowerCase();
 
-    // if (e.id =="rock") {
-    //     player = 1 
-    // } else if (e.id =="paper") {
-    //     player = 2
-    // } else if (e.id =="scissor"){
-    //     player = 3
-    // }
+    if (e.id =="rock") {
+        player = 1 
+    } else if (e.id =="paper") {
+        player = 2
+    } else if (e.id =="scissor"){
+        player = 3
+    }
 
 
-    // if (player == computerin){
-    //     return "IT'S A TIE";
-    // }else if (player != computerin) {
+    if (player == computerin){
+        return "IT'S A TIE";
+    }else if (player != computerin) {
 
-    //     if (player == 1 && computerin == 2 || player == 2 && computerin == 3 || player == 3 && computerin ==1 ) {
-    //         return "YOU LOST";
-    //     } else {
-    //         return "YOU WON";
-    //     }
+        if (player == 1 && computerin == 2 || player == 2 && computerin == 3 || player == 3 && computerin ==1 ) {
+            console.log("YOU LOST")
+            return "YOU LOST";
+        } else {
+            console.log("YOU WON")
+            return "YOU WON";
+        }
 
-    // }
+    }
 
-
-    return "lol"
 
 }
 
 
   
 // const playerSelection = "paper";
-const computerSelection = computerPlay();
+var computerSelection = computerPlay();
 // console.log(computerSelection);
 // console.log(playRound(playerSelection, computerSelection));
 
 const buttons = document.querySelectorAll('button');
 var list_Button = Array.from(buttons)
 
-list_Button.forEach(btn => btn.addEventListener('click',e=> {
-    return playRound(e, computerSelection);
-}));
+list_Button.forEach(btn => btn.addEventListener('click',e=> playRound(e, computerSelection)
+));

a4d483b23cffe1f93eb2b20537a86ae8c8dfc54c
Steven Pak <steven.pak10@gmail.com>
2022-06-21 23:02:15 -0400

    added event listener for buttons and added a big title

--- 04_rock_paper_scissors/javascript.js
+++ 04_rock_paper_scissors/javascript.js
@@ -22,7 +22,7 @@ function computerPlay(){
 }
 
 
-function playRound(playerin, computerin) {
+function playRound(e, computerin) {
 
 
 
@@ -31,34 +31,46 @@ function playRound(playerin, computerin) {
     var result = "";
 
     //needed to make sure that the player's input is all different capitalizations.
-    playerin.toLowerCase();
+    console.log(e.id)
+    // playerin = e.id
+    // playerin.toLowerCase();
 
-    if (playerin =="rock") {
-        player = 1 
-    } else if (playerin =="paper") {
-        player = 2
-    } else if (playerin =="scissor"){
-        player = 3
-    }
-
-
-    if (player == computerin){
-        return "IT'S A TIE";
-    }else if (player != computerin) {
-
-        if (player == 1 && computerin == 2 || player == 2 && computerin == 3 || player == 3 && computerin ==1 ) {
-            return "YOU LOST";
-        } else {
-            return "YOU WON";
-        }
-
-    }
+    // if (e.id =="rock") {
+    //     player = 1 
+    // } else if (e.id =="paper") {
+    //     player = 2
+    // } else if (e.id =="scissor"){
+    //     player = 3
+    // }
 
 
+    // if (player == computerin){
+    //     return "IT'S A TIE";
+    // }else if (player != computerin) {
+
+    //     if (player == 1 && computerin == 2 || player == 2 && computerin == 3 || player == 3 && computerin ==1 ) {
+    //         return "YOU LOST";
+    //     } else {
+    //         return "YOU WON";
+    //     }
+
+    // }
+
+
+    return "lol"
 
 }
+
+
   
-const playerSelection = "paper";
+// const playerSelection = "paper";
 const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// console.log(computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+const buttons = document.querySelectorAll('button');
+var list_Button = Array.from(buttons)
+
+list_Button.forEach(btn => btn.addEventListener('click',e=> {
+    return playRound(e, computerSelection);
+}));

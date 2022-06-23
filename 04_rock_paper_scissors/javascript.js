40b4fb889eb632c8dbdfc78b7920b864b3cd9468
Steven Pak <steven.pak10@gmail.com>
2022-06-22 21:43:39 -0400

    finished the better version of RPS wesbite

--- 04_rock_paper_scissors/javascript.js
+++ 04_rock_paper_scissors/javascript.js
@@ -10,17 +10,16 @@ function computerPlay(){
     return randnum; 
 }
 
+let win_counter = 0
+let lose_counter = 0
+let counter = 0
 
 function playRound(e) {
+
+    counter++
     var player   = 0;
     var computerin ="";
-    var result = "";
     computerin = computerPlay()
-    //needed to make sure that the player's input is all different capitalizations.
-    // console.log(e.target.id)
-    // console.log(computerin)
-    // playerin = e.id
-    // playerin.toLowerCase();
 
     if (e.target.id ==="rock") {
         player = 1 
@@ -30,20 +29,60 @@ function playRound(e) {
         player = 3
     }
 
-    // console.log(player)
+    let result = document.createElement(`div`)
+    result.setAttribute('id','result')
+    result.setAttribute('style', "padding: 50px;font-size: 100px;font-weight: bold;display: flex; align-items: center; justify-content: center; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif ; ")
+
+    let mark = document.createElement('div')
+    mark.setAttribute('id','mark')
+    mark.setAttribute('style', "font-size: 100px;font-weight: bold;display: flex; align-items: center; justify-content: center; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif ; ")
+
+
+    let delresult = document.querySelector('#result')
+    let delresult_mark = document.querySelector('#mark')
+    if (delresult !== null && delresult_mark !== null ){
+        delresult.remove()
+        delresult_mark.remove()
+    }
+
+
 
     if (player == computerin){
-        console.log("IT'S A TIE")
-        return "IT'S A TIE";
+        mark.textContent = 'Wins: ' + win_counter + ' Lost: ' + lose_counter 
+        result.textContent = 'GAME: '+counter + ' | You tied!'
+        const choice = document.querySelector('body');
+        choice.appendChild(result)
+        choice.appendChild(mark)
+        
     }else if (player != computerin) {
 
         if (player == 1 && computerin == 2 || player == 2 && computerin == 3 || player == 3 && computerin ==1 ) {
-            console.log("YOU LOST")
-            return "YOU LOST";
+            lose_counter++
+
+            mark.textContent = 'Wins: ' + win_counter + ' Lost: ' + lose_counter 
+            result.textContent = 'GAME: '+counter + ' | You lost!'
+            const choice = document.querySelector('body');
+            choice.appendChild(result)
+            choice.appendChild(mark)
+      
         } else {
-            console.log("YOU WON")
-            return "YOU WON";
+            win_counter++
+            mark.textContent = 'Wins: ' + win_counter + ' Lost: ' + lose_counter 
+            result.textContent = 'GAME: '+counter + ' | You won!'
+            const choice = document.querySelector('body');
+            choice.appendChild(result)
+            choice.appendChild(mark)
+          
         }
+
+    }
+
+    if (counter === 5){
+        delresult.remove()
+        delresult_mark.remove()
+        win_counter = 0
+        lose_counter = 0
+        counter = 0
 
     }
 }
